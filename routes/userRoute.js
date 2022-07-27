483b3745bfe81f551da53c45808c7838f56e0b16
TristanDaniels <tristandaniels07@gmail.com>
2022-07-27 14:57:38 +0200

    first commit

---
+++ routes/userRoute.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const router = express.Router();
+const con = require("../lib/db_connection");
+
+router.get("/", (req, res) => {
+  try {
+    con.query("SELECT * FROM users", (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+router.post("/", (req, res) => {
+  const {
+    email,
+    password,
+    full_name,
+    billing_address,
+    default_shipping_address,
+    country,
+    phone,
+    user_type,
+  } = req.body;
+  try {
+    con.query(
+      `INSERT INTO users (email, password, full_name, billing_address, default_shipping_address, country, phone, user_type) values ('${email}',
+      '${password}',
+      '${full_name}',
+      '${billing_address}',
+      '${default_shipping_address}',
+      '${country}',
+      '${phone}',
+      '${user_type}')`,
+
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Delete by id
+router.delete("/:id", (req, res) => {
+  const { user_id } = req.params;
+  try {
+    con.query(
+      `DELETE FROM users WHERE id=${user_id}`,
+
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;

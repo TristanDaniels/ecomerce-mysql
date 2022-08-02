c8d1086ecaff513f3acca64d65bf3b3ba0e2f888
TristanDaniels <tristandaniels07@gmail.com>
2022-08-02 09:29:27 +0200

    first commit

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
 const express = require("express"); // Used to set up a server
 const cors = require("cors"); // Used to prevent errors when working locally
-const middleware = require("../middleware/auth");
-
 const app = express(); // Initialize express as an app variable
 app.set("port", process.env.PORT || 6969); // Set the port
 app.use(express.json()); // Enable the server to handle JSON requests
 app.use(cors()); // Dont let local development give errors
-
-// import routes
-const userRoute = require("./routes/userRoute");
-const productRoute = require("./routes/productRoute");
-const orderRoute = require("./routes/orderRoute");
-const categoryRoute = require("./routes/categoryRoute");
-const order_detailRoute = require("./routes/order_detailRoute");
-const product_categoryRoute = require("./routes/product_categoryRoute");
-
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome" });
 });
-
+//for user
+const userRoute = require("./routes/userRoute");
 app.use("/users", userRoute);
+//for product
+const productRoute = require("./routes/productRoute");
 app.use("/products", productRoute);
-app.use("/orders", orderRoute);
+//for categories
+const categoryRoute = require("./routes/categoryRoute");
 app.use("/categories", categoryRoute);
+//for orders
+const orderRoute = require("./routes/orderRoute");
+app.use("/orders", orderRoute);
+//for order_details
+const order_detailRoute = require("./routes/order_detailRoute");
 app.use("/order_details", order_detailRoute);
+//for product_categories
+const product_categoryRoute = require("./routes/product_categoryRoute");
 app.use("/product_categories", product_categoryRoute);
-
 app.listen(app.get("port"), () => {
   console.log(`Listening for calls on port ${app.get("port")}`);
   console.log("Press Ctrl+C to exit server");
 });
-// const jwt = require("jsonwebtoken");
-// const bcrypt = require("")
 app.use(express.static("public"));
-
 app.get("/", function (req, res) {
-  res.sendFile(__dirnamev + "/" + "index.html");
+  res.sendFile(_dirname + "/" + "index.html");
 });
